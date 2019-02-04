1f940c0cf06b8e7f13d7cad60a138f7978b538e7
Bastien Roques <bastien.roques@talis-bs.net>
2019-02-04 15:34:51 +0100

    add session

--- login.php
+++ login.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog Talis</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
+    <nav>
+        <a href="disconnect.php">Disconnect</a>
+    </nav>
     <form action="login_post.php" method="post">
-        <input type="text" placeholder="Username" name="username">
+        <input type="text" placeholder="Pseudo" name="pseudo">
         <input type="password" placeholder ="Password" name="password">
         <input type="submit" value="Connect">
     </form>
@@ -18,9 +20,5 @@
             echo $_GET["message"];
         }
     ?>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
--- login_post.php
+++ login_post.php
@@ -1,12 +1,11 @@
 <?php
-    var_dump($_GET);
-    var_dump($_POST);
-
     if($_POST["pseudo"] === "admin" && $_POST["password"] === "admin") {
-        echo "Bonjour " . $_GET["pseudo"];
+        session_start();
+        $_SESSION["pseudo"] = $_POST["pseudo"];
+        header("Location: admin.php");
     }elseif($_POST["password"] !== "admin"){
-        header("Location: login.php?message=wrong password");
+        header("Location: login.php?message=Erreur de password");
     }elseif($_POST["pseudo"] !== "admin"){
-        header("Location: login.php?message=wrong username");
+        header("Location: login.php?message=Erreur de pseudo");
     }
 ?>
