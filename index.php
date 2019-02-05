1289a6ab93e9a7d31dca262f820f75fd0b16fc7e
Bastien Roques <bastien.roques@talis-bs.net>
2019-02-05 09:02:13 +0100

    add home start

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php require "nav-bar.php"; ?>
-</body>
-</html>
+<?php
+session_start();
+require 'nav_bar.php';
+?>
